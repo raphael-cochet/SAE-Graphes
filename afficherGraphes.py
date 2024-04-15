a85ec8fff07ea83915be6b309f4b1cc8570d0c39
Cochet Raphael <raphael.cochet1234@outlook.fr>
2024-04-15 12:04:41 +0200

    Ajout de la documentation

--- afficherGraphes.py
+++ afficherGraphes.py
@@ -3,6 +3,13 @@ import json
 import matplotlib.pyplot as plt
 
 def construire_graphe(nom_fichier):
+    '''
+    Construit un graphe à partir d'un fichier JSON contenant des films et leurs acteurs.
+    Args:
+        nom_fichier (str): Le nom du fichier JSON.
+    Returns:
+        nx.Graph: Un graphe NetworkX où les nœuds sont les acteurs et les arêtes représentent les collaborations entre acteurs dans des films.
+    '''
     G = nx.Graph()
 
     with open(nom_fichier) as json_load:
@@ -17,6 +24,11 @@ def construire_graphe(nom_fichier):
     return G
 
 def afficher_graphe(G):
+    '''
+    Affiche le graphe en utilisant la bibliothèque Matplotlib.
+    Args:
+        G (nx.Graph): Le graphe à afficher.
+    '''
     plt.figure(figsize=(10, 8))
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, node_size=35, node_color="skyblue", edge_color="gray", alpha=0.7, font_size=5)
@@ -34,7 +46,6 @@ def collaborateur_en_commun(acteur1, acteur2, G):
     '''
     if acteur1 not in G.nodes or acteur2 not in G.nodes:
         return set()
-
     voisins_acteur1 = set(G.neighbors(acteur1))
     voisins_acteur2 = set(G.neighbors(acteur2))
 
