b58c0314785eed72b3b16d3a9950c365ab15cf7d
Rickeur <raphael.cochet1234@outlook.fr>
2024-05-27 12:14:39 +0200

    Modif de Fonction et Ececutable

--- Executable.py
+++ Executable.py
@@ -1,14 +1,14 @@
-from afficherGraphes import *
+from fonctions import *
 
-
-titre = "Programme python"
+titre = "A la conquête d’Hollywood"
 liste_options = [
-    "Afficher graphe petit",
-    "Afficher graphe moyen",
-    "Afficher graphe complet",
+    "Afficher le graphe",
     "Collaborateurs en communs",
-    "Collaborateurs en communs TEST",
-    ]
+    "Collaborateur proche",
+    "Distance entre acteurs",
+    "Excentricite d'un acteur",
+    "Trouver l'acteur central",
+]
 
 def afficher_menu(titre, liste_options):
     print("+" + "-" * (len(titre)+2) + "+")
@@ -22,37 +22,45 @@ def afficher_menu(titre, liste_options):
     return rep
 
 def programme_principal():
+    fichier = input("Entrez un fichier à charger (sans le .json): ")
+    try:
+        G = construire_graphe(fichier+".json")
+    except Exception as e:
+        print("Erreur lors du chargement du fichier:", e)
+        return
+    
     while True:
         rep = afficher_menu(titre, liste_options)
         if rep is None:
             print("Cette option n'existe pas")
         elif rep == '1':
-            print("Vous avez choisi :",liste_options[int(rep) - 1])
-            afficher_graphe(construire_graphe("dataTest1.json"))
+            print("Vous avez choisi :", liste_options[int(rep) - 1])
+            afficher_graphe(G)
         elif rep == '2':
             print("Vous avez choisi :", liste_options[int(rep) - 1])
-            afficher_graphe(construire_graphe("dataTest2.json"))
+            acteur1 = input("Entrez le nom de l'acteur 1: ")
+            acteur2 = input("Entrez le nom de l'acteur 2: ")
+            print("Les collaborateurs en commun de", acteur1, "et", acteur2, "sont :", collaborateur_en_commun(acteur1, acteur2, G))
         elif rep == '3':
             print("Vous avez choisi :", liste_options[int(rep) - 1])
-            print("Bonne chance, pour patienter :", "https://www.youtube.com/watch?v=6R7ykH1movg")
-            afficher_graphe('data.json')
+            acteur = input("Entrez le nom de l'acteur: ")
+            k = int(input("Entrez la distance maximale: "))
+            print("Les acteurs proches de", acteur, "sont :", collaborateur_proche(acteur, k, G))
         elif rep == '4':
             print("Vous avez choisi :", liste_options[int(rep) - 1])
             acteur1 = input("Entrez le nom de l'acteur 1: ")
             acteur2 = input("Entrez le nom de l'acteur 2: ")
-            fichier = input("Entrez un fichier: ")
-            graphe = construire_graphe(fichier)
-            collaborateurs = collaborateur_en_commun(acteur1, acteur2, graphe)
-            print("Les collaborateurs en commun de", acteur1, "et", acteur2, "sont :", collaborateurs)
+            print("La distance entre", acteur1, "et", acteur2, "est :", distance_entre_acteurs(acteur1, acteur2, G))
         elif rep == '5':
-            print("Vous avez choisi :", liste_options[int(rep) - 1])
-            graphe = construire_graphe("dataTest1.json")
-            collaborateurs = collaborateur_en_commun("ACTEUR 1", "ACTEUR 3", graphe)
-            print("Les collaborateurs en commun de", acteur1, "et", acteur2, "sont :", collaborateurs)
+            acteur = input("Entrez le nom de l'acteur: ")
+            print("L'excentricité de:", acteur, "est", excentricite(acteur, G))
+        elif rep == '6':
+            print("L'acteur le plus central du graphe est:", acteur_central(G))
         elif rep == 'q':
-            print("Merci au revoir!")
+            print("Merci, au revoir!")
             break
         else:
-            print("Votre réponse n'est pas entre 1 et",str(len(liste_options)))
-        input("Appuyer sur Entrée pour retourer au menu ")
+            print("Votre réponse n'est pas entre 1 et", str(len(liste_options)))
+        input("Appuyer sur une touche pour retourner au menu ")
+
 programme_principal()
